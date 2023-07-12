232dc94be0c7c09e97632e19a978e6b67b3b26b4
TetAn <tetan27@yandex.ru>
2023-07-12 21:01:31 +0700

    feat: Создать класс-модель User

--- src/main/java/ru/practicum/shareit/user/User.java
+++ src/main/java/ru/practicum/shareit/user/User.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.user;
 
-/**
- * TODO Sprint add-controllers.
- */
+import lombok.Data;
+import lombok.NonNull;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+@Data
 public class User {
+    private int id;
+    private String name;
+    @Email(message = "Некорректный email")
+    @NotEmpty(message = "email не может быть пустым")
+    @NonNull
+    private String email;
 }
