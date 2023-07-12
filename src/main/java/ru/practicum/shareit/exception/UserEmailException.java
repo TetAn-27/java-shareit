79abb24d94710f3b119a3dc9e49396398106b225
TetAn <tetan27@yandex.ru>
2023-07-12 22:26:33 +0700

    feat: Создать обработчики исключений

---
+++ src/main/java/ru/practicum/shareit/exception/UserEmailException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class UserEmailException extends RuntimeException {
+    public UserEmailException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/ru/practicum/shareit/exception/UserIdException.java
@@ -0,0 +1,7 @@
+package ru.practicum.shareit.exception;
+
+public class UserIdException extends RuntimeException {
+    public UserIdException(String message) {
+        super(message);
+    }
+}
