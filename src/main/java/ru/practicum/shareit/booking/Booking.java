e60737f9ab605e9151e201bcfb9bb463c1f36a99
TetAn <tetan27@yandex.ru>
2023-07-26 19:24:30 +0700

    feat: Настроить JPA при помощи анотаций для Booking

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -4,14 +4,26 @@ import lombok.Data;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.User;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "bookings")
 public class Booking {
+
+    @Id
     private Integer id;
+    @Column(name = "start_date", nullable = false)
     private LocalDateTime start;
+    @Column(name = "end_date", nullable = false)
     private LocalDateTime end;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id", nullable = false)
     private Item item;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "booker_id", nullable = false)
     private User booker;
+    @Enumerated
     private Status status;
 }
