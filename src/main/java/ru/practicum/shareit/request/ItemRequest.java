dcc83041b8d2e0d379982b5745e0f034edc25072
TetAn <tetan27@yandex.ru>
2023-07-26 19:29:32 +0700

    feat: Настроить JPA при помощи анотаций для ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -3,14 +3,21 @@ package ru.practicum.shareit.request;
 import lombok.Data;
 import ru.practicum.shareit.user.User;
 
+import javax.persistence.*;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 
 @Data
+@Entity
+@Table(name = "requests")
 public class ItemRequest {
+    @Id
     private Integer id;
     @Size(max = 200, message = "Описание фильма превышает 200 символов")
     private String description;
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "requester_id", nullable = false)
     private User requester;
+    @Column(name = "created_date", nullable = false)
     private LocalDateTime created;
 }
