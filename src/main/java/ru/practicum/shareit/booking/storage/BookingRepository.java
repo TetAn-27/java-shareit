7dd924c51fa9978e5b7e2dc424e878b63d8aff91
TetAn <tetan27@yandex.ru>
2023-07-29 14:01:22 +0700

    feat: Создать BookingRepository

--- src/main/java/ru/practicum/shareit/booking/storage/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/storage/BookingRepository.java
@@ -2,7 +2,18 @@ package ru.practicum.shareit.booking.storage;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.practicum.shareit.booking.Booking;
-import ru.practicum.shareit.item.model.Item;
+
+import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Integer> {
+
+    @Override
+    Booking save(Booking booking);
+
+    @Override
+    Booking getById(Integer bookingId);
+
+    List<Booking> findAllByBookerId(int userId);
+
+    List<Booking> findAllByOwnerId(int userId);
 }
