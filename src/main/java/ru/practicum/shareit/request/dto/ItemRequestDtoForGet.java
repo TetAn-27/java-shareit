40a022558fb8f745d70cee552b342c6a39a64acb
TetAn <tetan27@yandex.ru>
2023-08-08 22:20:49 +0700

    feat: Создать класс Dto и Mapper

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoForGet.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDtoForGet.java
@@ -2,15 +2,18 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.practicum.shareit.item.dto.ItemDto;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
-public class ItemRequestDto {
+public class ItemRequestDtoForGet {
     private Integer id;
     @NotNull
     private String description;
     private LocalDateTime created;
+    private List<ItemDto> items;
 }
