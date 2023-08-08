1ebfe7700bbc12f52a17fef5c79897df21d0c275
TetAn <tetan27@yandex.ru>
2023-08-08 20:47:01 +0700

    feat: Создать класс ItemRequestController

--- src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -1,12 +1,42 @@
 package ru.practicum.shareit.request;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.practicum.shareit.request.dto.ItemRequestDto;
 
-/**
- * TODO Sprint add-item-requests.
- */
+import javax.validation.Valid;
+import java.util.List;
+
 @RestController
 @RequestMapping(path = "/requests")
 public class ItemRequestController {
+    private final ItemRequestService itemRequestService;
+
+    public ItemRequestController(ItemRequestService itemRequestService) {
+        this.itemRequestService = itemRequestService;
+    }
+
+    @PostMapping
+    public ItemRequestDto create(@RequestHeader("X-Sharer-User-Id") Integer userId,
+                                 @Valid @RequestBody ItemRequestDto itemRequestDto) {
+        return itemRequestService.create(userId, itemRequestDto).get();
+    }
+
+    @GetMapping
+    public List<ItemRequestDto> findAll(@RequestHeader("X-Sharer-User-Id") Integer userId) {
+        return itemRequestService.findAll(userId);
+    }
+
+    @GetMapping("/all") //GET /requests/all?from={from}&size={size}
+    public List<ItemRequestDto> getAllRequests(@RequestHeader("X-Sharer-User-Id") Integer userId,
+                                               @RequestParam(value = "from", defaultValue = "0", required = false)
+                                                    Integer from,
+                                               @RequestParam(value = "size", defaultValue = "10", required = false)
+                                                   Integer size) {
+        return itemRequestService.getAllRequests(userId, from, size);
+    }
+
+    @GetMapping("/{requestId}") //GET /requests/{requestId}
+    public ItemRequestDto getItemRequestById(@PathVariable("requestId") Integer id) {
+        return itemRequestService.getItemRequestById(id).get();
+    }
 }
--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -2,8 +2,11 @@ package ru.practicum.shareit.request.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import ru.practicum.shareit.item.dto.ItemDto;
 
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -11,5 +14,7 @@ public class ItemRequestDto {
     private Integer id;
     @NotNull
     private String description;
+    private LocalDateTime created;
+    private List<ItemDto> items;
 
 }
